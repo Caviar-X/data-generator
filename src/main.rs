60b8becda6ed4f992a54ff22bc5d5440482fac5f
Caviar-X <caviarx@163.com>
2022-06-11 11:59:31 +0800

    inital commit : release 1.0.0

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
+use clap::Parser as ClapParser;
+use colorful::*;
 use data_generator::parser::Parser;
+use std::fs::File;
+use std::io::Write;
+/// A simple DSL which allows you to create data simple and fast
+#[derive(ClapParser, Debug, Clone)]
+#[clap(author = "Caviar-X",version = "1.0.0",about,long_about = None)]
+pub struct Args {
+    /// The generate times
+    #[clap(short, long, default_value_t = 10)]
+    pub count: u8,
+    /// The name of the file
+    #[clap(short, long)]
+    pub file: String,
+}
 fn main() {
-    let p = Parser::new_with_context("{int from 0 to 1e5}".into());
-    println!("{:?}",p.next_token());
+    let params: Args = Args::parse();
+    for i in 1..=params.count {
+        let mut data: File = File::create(format!("{}.in", i)).expect("Failed to create file");
+        let mut parser: Parser = Parser::new(params.clone().file);
+        loop {
+            let clone = parser.clone();
+            let token: Option<Vec<&str>> = parser.next_token();
+            if token == None {
+                break;
+            }
+            if let Some(t) = token {
+                write!(data, "{} ", clone.parse(t).unwrap()).expect("Failed to write data");
+            }
+        }
+        eprintln!("{}", format!("Generating file {}.in", i).color(Color::Blue));
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,9 +1,12 @@
+use anyhow::*;
+use colorful::*;
 use rand::prelude::*;
 use std::fs::*;
+use std::iter::repeat_with;
 #[derive(Clone, Debug)]
 pub struct Parser {
     pub file: Option<String>,
-    position: usize,
+    pub position: usize,
     context: String,
 }
 impl Parser {
@@ -22,45 +25,108 @@ impl Parser {
         }
     }
     pub fn next_token(&mut self) -> Option<Vec<&str>> {
-        if self.position == self.context.len() {
+        if self.position == (self.context.len() - 1) {
             return None;
         }
         let substr: &str = self
             .context
-            .get(self.position..self.context.len())
+            .get(self.position..)
             .expect("Failed to get token");
         let (l, r) = (
             substr.find('{').expect("Failed to get token") + 1,
             substr.find('}').expect("Failed to get token"),
         );
-        self.position += (r - l) + 2;
+        self.position += (r - l) + 2; //plus {}
         Some(
             substr
-                .get(l..r)
+                .get((l)..(r))
                 .expect("Failed to get token")
                 .split_whitespace()
                 .collect::<Vec<&str>>(),
         )
     }
-    fn parse(&self, tokens: Vec<&str>) -> Option<String> {
+    fn is_vailid_token(&self, token: &str) -> bool {
+        self.parse(vec![token]).is_ok() || token.parse::<f64>().is_ok()
+    }
+    pub fn parse(&self, tokens: Vec<&str>) -> Result<String> {
         if tokens.len() != 1 {
             return self.parse_statements(tokens);
         }
         match tokens[0] {
-            "short" | "i16" => random::<i16>().to_string(),
-            "int" | "i32" => random::<i32>().to_string(),
-            "long long" | "i64" => random::<i64>().to_string(),
-            "int128_t" | "i128" => random::<i128>().to_string(),
-            "unsigned short" | "u16" => random::<u16>().to_string(),
-            "unsigned int" | "u32" => random::<u32>().to_string(),
-            "unsigned long long" | "u64" => Some(random::<u64>().to_string()),
-            "uint128_t" | "u128" => Some(random::<u128>().to_string()),
-            _ => {
-                None
+            "short" | "i16" => Ok(random::<i16>().to_string()),
+            "int" | "i32" => Ok(random::<i32>().to_string()),
+            "i64" => Ok(random::<i64>().to_string()),
+            "int128_t" | "i128" => Ok(random::<i128>().to_string()),
+            "u16" => Ok(random::<u16>().to_string()),
+            "u32" => Ok(random::<u32>().to_string()),
+            "u64" => Ok(random::<u64>().to_string()),
+            "uint128_t" | "u128" => Ok(random::<u128>().to_string()),
+            "bigint" => {
+                let mut iter = repeat_with(|| thread_rng().gen_range(48_u8..57_u8) as char)
+                    .take(random::<u16>() as usize);
+                let num = iter.position(|x| x != '0');
+                return Ok(if num == None {
+                    "0".into()
+                } else {
+                    iter.clone().collect::<Vec<char>>()[num.unwrap() as usize..]
+                        .iter()
+                        .collect::<String>()
+                });
             }
+            "string" => Ok(
+                repeat_with(|| thread_rng().gen_range(20_u8..126_u8) as char)
+                    .take(random::<u16>() as usize)
+                    .collect::<String>(),
+            ),
+            "char" => Ok(random::<char>().to_string()),
+            _ => bail!(
+                "{}",
+                format!(
+                    "failed to parse at position {} : read undeclared tokens",
+                    self.position + 1
+                )
+                .color(Color::Red)
+                .bold()
+            ),
         }
     }
-    fn parse_statements(&self, tokens: Vec<&str>) -> String {
-        todo!()
+    pub fn parse_statements(&self, tokens: Vec<&str>) -> Result<String> {
+        let mut ret: Result<String> = Ok(String::new());
+        for (c, i) in tokens.clone().into_iter().enumerate() {
+            if ret.is_err() {
+                break;
+            }
+            match i {
+                "to" => {
+                    ret = Ok(thread_rng()
+                        .gen_range(
+                            tokens[c - 1].parse::<u128>().expect("Failed to parse")
+                                ..tokens[c + 1].parse::<u128>().expect("Failed to parse"),
+                        )
+                        .to_string());
+                }
+                _ => {
+                    if !self.is_vailid_token(i) {
+                        ret = Err(anyhow!(
+                            "{}",
+                            format!(
+                                "failed to parse at position {} : read undeclared tokens",
+                                self.position + 1
+                            )
+                            .color(Color::Red)
+                            .bold()
+                        ));
+                    }
+                }
+            }
+        }
+        ret
+    }
+}
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_parse() {
+        todo!();
     }
 }
